023a631e12867e92ff7841fe9ea47db63b6b694f
AndreanaPerla <andreana.perla@gmail.com>
2026-01-10 17:00:04 +0100

    fix Gallery

--- photo-gallery/src/views/Gallery.vue
+++ photo-gallery/src/views/Gallery.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <!-- <h1 class="text-3xl font-bold text-gray-800 mb-6">Galleria Fotografica</h1>
-
-    <div v-if="isLoading" class="text-center">
-      <p class="text-lg text-gray-600">Caricamento foto...</p>
+  <div class="container m-auto">
+    <div v-if="isLoading" class="loading">
+      <p>Caricamento foto...</p>
     </div>
 
     <div v-if="error" class="text-center text-red-600">
@@ -12,27 +10,21 @@
 
     <div
       v-if="photos.length > 0"
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
     >
       <router-link
         v-for="photo in photos"
         :key="photo.id"
         :to="{ name: 'PhotoDetail', params: { id: photo.id } }"
-        class="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300 cursor-pointer block"
+        class="photo"
       >
         <img
           :src="photo.url"
           :alt="photo.fileName"
-          class="w-full h-48 object-cover"
+          class="photo-image"
         />
-        <div class="p-4">
-          <h3 class="text-sm font-semibold text-gray-800 truncate">
-            {{ photo.fileName.split(".")[0] }}
-          </h3>
-          <p class="text-xs text-gray-600 mt-1">{{ photo.fileSize }}</p>
-        </div>
       </router-link>
-    </div> -->
+    </div>
   </div>
 </template>
 
